d7343ff28a43562d572be65efa1d0dc7b6dca57a
Nithinkrishna123 <113376297+Nithinkrishna123@users.noreply.github.com>
2022-10-26 17:03:14 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,7 +1,7 @@
 const app=require("express")();
 const PORT=process.env.PORT || 3000;
 app.get("",(req,res)=>{
-    res.send("HI Valluru NITHIN");
+    res.send("HI Welcome to special team");
 });
 app.listen(PORT, ()=>{
     console.log('app up at port ${PORT}');
